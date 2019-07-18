6af7e6c0289d60d786a7239d087b3c9619a1491a
IvyGao58 <767017547@qq.com>
2019-07-18 11:44:08 +0800

    整数中1出现的次数

---
+++ algorithm/array/NumberOf1Between1AndN_Solution.js
@@ -0,0 +1,29 @@
+/**
+ * @topic: 求出1~13的整数中1出现的次数,并算出100~1300的整数中1出现的次数？为此他特别数了一下1~13中包含1的数字有1、10、11、12、
+ * 13因此共出现6次,但是对于后面问题他就没辙了。ACMer希望你们帮帮他,并把问题更加普遍化,可以很快的求出任意非负整数区间中1出现的次数
+ * （从1 到 n 中1出现的次数）。
+ * @Date: 2019-07-18
+ */
+
+function numberOf1Between1AndN_Solution(n)
+{
+    if (n <= 0) {
+        return 0;
+    }
+    let array = generate(n);
+    array = array.sort();
+    let reducer = array.reduce((count, item) => {
+        let str = item.toString();
+        let strs = str.split('');
+        strs.forEach(item => item === '1' ? count += 1 : count);
+        return count;
+    });
+    return reducer;
+}
+
+function generate(n) {
+    return Array.from({length: n}, (key, value) => value + 1);
+}
+
+let value = numberOf1Between1AndN_Solution(1);
+console.log(value);
